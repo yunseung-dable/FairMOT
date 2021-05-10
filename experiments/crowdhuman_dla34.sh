ca367dae4f99dd73676a85bbc66a6f9437fe6d57
yunseung-dable <yunseung@dable.io>
2021-05-10 17:54:46 +0900

    reducing batch_size

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id crowdhuman_dla34 --gpus -1  --batch_size 2 --num_epochs 60 --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
+python train.py mot --exp_id crowdhuman_dla34 --gpus -1  --batch_size 1 --num_epochs 60 --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
 cd ..
