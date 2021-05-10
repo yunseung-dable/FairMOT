33b511e6ded2ad7a8184895c2f6a8bd7c27bea02
yunseung-dable <yunseung@dable.io>
2021-05-10 10:42:56 +0900

    debugging

--- src/train.py
+++ src/train.py
@@ -59,11 +59,11 @@ def main(opt):
     Trainer = train_factory[opt.task]
     trainer = Trainer(opt, model, optimizer)
     trainer.set_device(opt.gpus, opt.chunk_sizes, opt.device)
-    print('passing through structure ')
+
     if opt.load_model != '':
         model, optimizer, start_epoch = load_model(
             model, opt.load_model, trainer.optimizer, opt.resume, opt.lr, opt.lr_step)
-
+    print('loading process finished ')
     for epoch in range(start_epoch + 1, opt.num_epochs + 1):
         mark = epoch if opt.save_all else 'last'
         log_dict_train, _ = trainer.train(epoch, train_loader)
