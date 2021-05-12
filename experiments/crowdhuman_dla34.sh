663ba19d841fe2e86f63d721de4bde88bc6f24f3
yunseung-dable <yunseung@dable.io>
2021-05-12 18:41:42 +0900

    clone-> detach().clone() in pose_dla_dcn.py

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id crowdhuman_dla34 --gpus 1,2,3  --batch_size 8 --num_epochs 60 --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
+python train.py mot --exp_id crowdhuman_dla34 --gpus 1,2,3  --batch_size 8 --num_epochs 60 --load_model '../exp/mot/crowdhuman_dla34/model_last.pth' --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
 cd ..
