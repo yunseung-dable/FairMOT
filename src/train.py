a2c8916a7a63ec8564ffd667e1142c6fc49b5b54
yunseung-dable <yunseung@dable.io>
2021-05-13 10:24:31 +0900

    test_module added

--- src/train.py
+++ src/train.py
@@ -48,7 +48,7 @@ def main(opt):
 
     # Get dataloader
     n_dataset = len(dataset)
-    print(f'this is dataset0 : {dataset[0]}')
+    print(f'this is dataset0 : {dataset[:2]}')
     dataset_train, dataset_test = dataset[ : int(n_dataset * 0.8)], dataset[int(n_dataset * 0.8) : ]
 
     train_loader = torch.utils.data.DataLoader(
