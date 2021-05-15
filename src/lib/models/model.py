48d8e1b9f4b1d9d2b0064d72787ad5192d66a792
yunseung-dable <yunseung@dable.io>
2021-05-15 12:41:59 +0900

    revive dead variables in model.forward

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -36,36 +36,36 @@ def load_model(model, model_path, optimizer=None, resume=False,
   checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
   print('trying to load in {}, epoch {}'.format(model_path, checkpoint['epoch']))
   state_dict_ = checkpoint['state_dict']
-  # state_dict = {}
+  state_dict = {}
 
   # convert data_parallal to model
-  # for k in state_dict_:
-  #   if k.startswith('module') and not k.startswith('module_list'):
-  #     state_dict[k[7:]] = state_dict_[k]
-  #   else:
-  #     state_dict[k] = state_dict_[k]
-  # model_state_dict = model.state_dict()
-  #
-  # # check loaded parameters and created model parameters
-  # msg = 'If you see this, your model does not fully load the ' + \
-  #       'pre-trained weight. Please make sure ' + \
-  #       'you have correctly specified --arch xxx ' + \
-  #       'or set the correct --num_classes for your own dataset.'
-  # for k in state_dict:
-  #   if k in model_state_dict:
-  #     if state_dict[k].shape != model_state_dict[k].shape:
-  #       print('Skip loading parameter {}, required shape{}, '\
-  #             'loaded shape{}. {}'.format(
-  #         k, model_state_dict[k].shape, state_dict[k].shape, msg))
-  #       state_dict[k] = model_state_dict[k]
-  #   else:
-  #     print('Drop parameter {}.'.format(k) + msg)
-  # for k in model_state_dict:
-  #   if not (k in state_dict):
-  #     print('No param {}.'.format(k) + msg)
-  #     state_dict[k] = model_state_dict[k]
-  # model.load_state_dict(state_dict, strict=False)
-  model.load_state_dict(state_dict_, strict=False)
+  for k in state_dict_:
+    if k.startswith('module') and not k.startswith('module_list'):
+      state_dict[k[7:]] = state_dict_[k]
+    else:
+      state_dict[k] = state_dict_[k]
+  model_state_dict = model.state_dict()
+
+  # check loaded parameters and created model parameters
+  msg = 'If you see this, your model does not fully load the ' + \
+        'pre-trained weight. Please make sure ' + \
+        'you have correctly specified --arch xxx ' + \
+        'or set the correct --num_classes for your own dataset.'
+  for k in state_dict:
+    if k in model_state_dict:
+      if state_dict[k].shape != model_state_dict[k].shape:
+        print('Skip loading parameter {}, required shape{}, '\
+              'loaded shape{}. {}'.format(
+          k, model_state_dict[k].shape, state_dict[k].shape, msg))
+        state_dict[k] = model_state_dict[k]
+    else:
+      print('Drop parameter {}.'.format(k) + msg)
+  for k in model_state_dict:
+    if not (k in state_dict):
+      print('No param {}.'.format(k) + msg)
+      state_dict[k] = model_state_dict[k]
+  model.load_state_dict(state_dict, strict=False)
+  # model.load_state_dict(state_dict_, strict=False)
   print('load complete successfully!')
 
   # resume optimizer parameters
