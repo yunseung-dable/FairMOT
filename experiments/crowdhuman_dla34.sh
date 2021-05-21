b3c322aef40d7718474b8633828f16d3bfe4119e
yunseung-dable <yunseung@dable.io>
2021-05-21 12:56:35 +0900

    after revert, model load rollback to original

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id crowdhuman_dla34 --gpus 0,1,2,3   --batch_size 8 --num_iters -1  --num_epochs 60 --load_model '/data/models/mot/fairmot/crowdhuman_dla34/model_15.pth' --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
+python train.py mot --exp_id crowdhuman_dla34 --gpus 0,1,2,3   --batch_size 8 --num_iters -1 --resume --num_epochs 60 --load_model '/data/models/mot/fairmot/crowdhuman_dla34/model_last.pth' --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
 cd ..
