e5c4d020cb1955ed0c3962705faccc4f5071c420
yunseung-dable <yunseung@dable.io>
2021-05-26 12:44:37 +0900

    new branch : dataloader 16

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id crowdhuman_dla34 --gpus 0,1,2,3   --batch_size 8 --num_iters 30  --num_epochs 60  --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
+python train.py mot --exp_id crowdhuman_dla34 --gpus 0,1,2,3   --batch_size 8 --num_iters -1  --num_epochs 60  --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
 cd ..
