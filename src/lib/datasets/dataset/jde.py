c3635a0be3d0c923a53ce6d4d01ddc832db9a38b
yunseung-dable <yunseung@dable.io>
2021-05-26 11:28:47 +0900

    new branch : arch, dataloader, loss complete

--- src/lib/datasets/dataset/jde.py
+++ src/lib/datasets/dataset/jde.py
@@ -389,6 +389,7 @@ class JointDataset(LoadImagesAndLabels):  # for training
         for ds, label_paths in self.label_files.items():
             max_index = -1
             for lp in label_paths:
+                print(lp)
                 lb = np.loadtxt(lp)
                 if len(lb) < 1:
                     continue
