3f0cd004cd5b8ce37988833109805b377a6dd5ed
yunseung-dable <yunseung@dable.io>
2021-05-28 15:24:49 +0900

    new branch : let's go demo!

--- src/lib/tracker/multitracker.py
+++ src/lib/tracker/multitracker.py
@@ -294,11 +294,10 @@ class JDETracker(object):
             id_feature = id_feature.squeeze(0)
             id_feature = id_feature.cpu().numpy()
 
-        print(f'head_dets : {head_dets}')
-        print(f'full_dets : {full_dets}')
         print(f'head_dets shape before post_process : {head_dets.shape}')
         print(f'full_dets shape before post_process : {full_dets.shape}')
-
+        print(f'head_dets contents : {head_dets}')
+        print(f'full_dets contents : {full_dets}')
         head_dets = self.post_process(head_dets, meta)
         full_dets = self.post_process(full_dets, meta)
         # dets = self.merge_outputs([dets])[1]
