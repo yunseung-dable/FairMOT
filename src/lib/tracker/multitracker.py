148ca824e41a11476512890e9cbad60e01607a11
yunseung-dable <yunseung@dable.io>
2021-06-03 10:21:17 +0900

    new branch : when activate -> is_activated TRue

--- src/lib/tracker/multitracker.py
+++ src/lib/tracker/multitracker.py
@@ -332,20 +332,20 @@ class JDETracker(object):
         full_dets = self.post_process(full_dets, meta)
 
         ed_mat = metrics.pairwise.euclidean_distances(full_dets[1], head_dets[1])
-        print('ed_output')
-        print(ed_mat)
-        print('normalized ed')
-        print(ed_mat/(self.opt.img_size[0] * self.opt.img_size[1]))
+        # print('ed_output')
+        # print(ed_mat)
+        # print('normalized ed')
+        ed_minus = 1 - ed_mat / (self.opt.img_size[0] * self.opt.img_size[1])
         # dist_argmin = np.argmin(ed_mat, axis=1)
         # print(f'ed_dist argmin : {dist_argmin}')
 
         iou_mat = matching.ious(full_dets[1], head_dets[1])
-        print('iou res!!')
-        print(iou_mat)
+        # print('iou res!!')
+        # print(iou_mat)
 
-        ed_iou_mat = ed_mat * iou_mat
-        print('Ed Iou matrix')
-        print(ed_iou_mat)
+        ed_iou_mat = ed_minus * iou_mat
+        # print('Ed Iou matrix')
+        # print(ed_iou_mat)
 
         max_value_axis1 = np.max(ed_iou_mat, axis=1)
         over_zero_idx = np.where(max_value_axis1 >0, True, False)
