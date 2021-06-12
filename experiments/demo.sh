691f3f63aa08ed140cdea1aac49274df6d1dd39a
yunseung-dable <yunseung@dable.io>
2021-06-12 16:05:32 +0900

    new branch : ed normalize by frame ed

--- experiments/demo.sh
+++ experiments/demo.sh
@@ -1,2 +1,2 @@
 cd src
-python demo.py mot --load_model /data/models/mot/fairmot/crowdhuman_dla34/model_60.pth --conf_thres 0.4 --K 100 --input-video /data/models/mot/test/pedestrian2.mp4 --output-root /data/models/mot/test/
+python demo.py mot --load_model /data/models/mot/fairmot/crowdhuman_dla34/model_60.pth --conf_thres 0.4 --K 100 --input-video /data/models/mot/test/Paris_crop.mp4 --output-root /data/models/mot/test/
