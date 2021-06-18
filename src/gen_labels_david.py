d575caea50b81431cf32013d17d4881c9d371a45
hyeseung-of-dable <hyeseung@dable.io>
2021-06-18 18:17:50 +0900

    david_893 annot -> text label로 저장

--- src/gen_labels_david.py
+++ src/gen_labels_david.py
@@ -15,6 +15,45 @@ def gen_ann_list(file_list, ann_root):
 		    files.append( os.path.join(ann_root, file.strip()+'.json') )
     return files
 
+def write_to_text(tid_curr: int, 
+                  img_width: int, 
+                  img_height: int, 
+                  labels: dict, 
+                  label_fpath: str):
+    """
+    labels: 
+    {
+        1: {
+            'head_box': { 'x': 862, 'y': 94,  'width': 15, 'height': 58 }, 
+            'visible_box': { 'x': 866, 'y': 93, 'width': 10, 'height': 13 },
+            },
+        ...
+    }
+    """
+    
+    for label in labels: # for each object
+        hbox_x = label['head_box']['x']
+        hbox_y = label['head_box']['y']
+        hbox_w = label['head_box']['width']
+        hbox_h = label['head_box']['height']
+
+        hbox_x += hbox_w / 2 # center x
+        hbox_y += hbox_h / 2 # center y
+
+        vbox_x = label['visible_box']['x']
+        vbox_y = label['visible_box']['y']
+        vbox_w = label['visible_box']['width']
+        vbox_h = label['visible_box']['height']
+
+        vbox_x += vbox_w / 2 # center x
+        vbox_y += vbox_h / 2 # center y
+
+        label_str = '0 {:d} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(
+                tid_curr, hbox_x / img_width, hbox_y / img_height, hbox_w / img_width, hbox_h / img_height, 
+                vbox_x / img_width, vbox_y / img_height, vbox_w / img_width, vbox_h / img_height)
+
+        with open(label_fpath, 'a') as f:
+            f.write(label)
 
 def gen_labels_david(file_list, label_root, ann_root):
     """
@@ -56,6 +95,7 @@ def gen_labels_david(file_list, label_root, ann_root):
 
         for j in range(len(anns)): # each object(head or visible)
             person_id = anns[j]['classification']['attributes'][0]['value'] if anns[j]['classification']['attributes'][0]['code'] == 'id' else anns[j]['classification']['attributes'][1]['value'] 
+            person_id = int(person_id)
             mode = anns[j]['classification']['code']
 
             if person_id not in labels.keys():
@@ -65,7 +105,8 @@ def gen_labels_david(file_list, label_root, ann_root):
                 labels[person_id]['head_box'] = anns[j]['label']['data']
             elif mode  == 'visible':
                 labels[person_id]['visible_box'] = anns[j]['label']['data']
-           
+
+        write_to_text(labels)
 
 if __name__ == '__main__':
     ann_root = '/mnt/sda1/user/data/DAVID_893/jsons'
