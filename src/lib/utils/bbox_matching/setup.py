a1b9c02637b63eb1ccd2921c0de42969d741e6ca
jingu <jingu@dable.io>
2021-06-22 09:25:21 +0900

    fix typo at bbox_matching setup file

--- src/lib/utils/bbox_matching/setup.py
+++ src/lib/utils/bbox_matching/setup.py
@@ -5,6 +5,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize("bbox_mathing.pyx"),
+    ext_modules = cythonize("bbox_matching.pyx"),
     include_dirs = [numpy.get_include()]
 )
